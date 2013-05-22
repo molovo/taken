9aade92a37f765b5fd821dd681661cd77a6d4110
molovo <hi@molovo.co.uk>
2013-05-22 18:09:39 +0100

    MOAR

--- posts.php
+++ posts.php
@@ -1,4 +1,5 @@
 <?php theme_include('header'); ?>
+	<h1><?php echo current_url(); ?></h1>
 	<?php if (current_url() == 'blog'): ?>
 		<script type="text/javascript">
 			window.location.href = <?php echo base_url(); ?> + 'category/blogs';
